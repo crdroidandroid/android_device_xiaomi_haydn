be82fdb2219cb78c2bfcb80c66de90a9031f782f
Never-Alive <livinglucifer0@gmail.com>
2026-04-12 12:16:34 +0530

    haydn: BoardConfig: Remove kernel config along with modules Now we are using kernel with inlined modules and different configs
    Signed-off-by: Never-Alive <livinglucifer0@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,18 +18,6 @@ TARGET_BOOTLOADER_BOARD_NAME := haydn
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/hidl/manifest.xml
 
-# Kernel
-TARGET_KERNEL_CONFIG += vendor/haydn_QGKI.config
-
-# Kernel modules
-BOOT_KERNEL_MODULES := \
-    focaltech_touch.ko \
-    hwid.ko \
-    msm_drm.ko \
-    xiaomi_touch.ko
-
-BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(BOOT_KERNEL_MODULES)
-
 # Partitions
 BOARD_DTBOIMG_PARTITION_SIZE := 25165824
 
